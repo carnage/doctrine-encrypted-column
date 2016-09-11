128264379d3724b11eddd4441748295b78def765
Carnage <t.carnage@gmail.com>
2016-09-11 15:05:19 +0100

    Improved handling of bad json

--- src/Dbal/EncryptedColumn.php
+++ src/Dbal/EncryptedColumn.php
@@ -36,14 +36,7 @@ class EncryptedColumn extends Type
             return null;
         }
 
-        $decoded = json_decode($value, true);
-
-        switch (json_last_error()) {
-            case JSON_ERROR_NONE:
-                break;
-            default:
-                throw ConversionException::conversionFailed($value, 'Could not decode Json');
-        }
+        $decoded = $this->decodeJson($value);
 
         return EncryptedColumnVO::fromArray($decoded);
     }
@@ -51,5 +44,37 @@ class EncryptedColumn extends Type
     public function convertToDatabaseValue($value, AbstractPlatform $platform)
     {
         return json_encode($value);
+    }
+
+    /**
+     * Based on: https://github.com/schmittjoh/serializer/blob/master/src/JMS/Serializer/JsonDeserializationVisitor.php
+     *
+     * @param $value
+     * @return mixed
+     * @throws ConversionException
+     */
+    private function decodeJson($value)
+    {
+        $decoded = json_decode($value, true);
+
+        switch (json_last_error()) {
+            case JSON_ERROR_NONE:
+                if (!is_array($decoded)) {
+                    throw ConversionException::conversionFailed($value, 'Json was not an array');
+                }
+                return $decoded;
+            case JSON_ERROR_DEPTH:
+                throw ConversionException::conversionFailed($value, 'Could not decode JSON, maximum stack depth exceeded.');
+            case JSON_ERROR_STATE_MISMATCH:
+                throw ConversionException::conversionFailed($value, 'Could not decode JSON, underflow or the nodes mismatch.');
+            case JSON_ERROR_CTRL_CHAR:
+                throw ConversionException::conversionFailed($value, 'Could not decode JSON, unexpected control character found.');
+            case JSON_ERROR_SYNTAX:
+                throw ConversionException::conversionFailed($value, 'Could not decode JSON, syntax error - malformed JSON.');
+            case JSON_ERROR_UTF8:
+                throw ConversionException::conversionFailed($value, 'Could not decode JSON, malformed UTF-8 characters (incorrectly encoded?)');
+            default:
+                throw ConversionException::conversionFailed($value, 'Could not decode Json');
+        }
     }
 }
